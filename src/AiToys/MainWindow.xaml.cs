7e0897ca720da1594c64a94422b2c6069b6c77e9
Grzegorz Gwóźdź <g.gwozdz@hotmail.com>
2025-03-02 23:17:49 +0100

    feat: update application startup to use Generic Host for WinUI
    - Changed the application startup method from the default to using Generic Host

--- src/AiToys/MainWindow.xaml.cs
+++ src/AiToys/MainWindow.xaml.cs
@@ -3,7 +3,7 @@ using Microsoft.UI.Xaml;
 namespace AiToys;
 
 /// <summary>
-/// An empty window that can be used on its own or navigated to within a Frame.
+/// Provides application-specific behavior to supplement the default Window class.
 /// </summary>
 internal sealed partial class MainWindow : Window
 {
